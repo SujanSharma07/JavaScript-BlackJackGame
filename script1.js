6ba32871c315ef9a3fd82024da11cc385c88c0ee
Sujan Sharma <sujansharma202@gmail.com>
2020-05-30 12:11:34 +0545

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,4 @@
+
 /* BlackJack Game */
 let name,value;
 let dummy = [];
@@ -40,110 +41,117 @@ let buttonclassifier1 =100;
 let win = 0;
 let loss = 0;
 let draw = 0;
+let botchoice;
+
+function hitfunction() {
+
+	botchoice = Math.random()*12;	
+	botchoice = Math.floor(botchoice);
+	show_image  = "<img src='blackjack_assets/images/"+dummy[botchoice].name+"'"+"style='height:78%; width:55%;padding-top:5px;'>";
+
+	let newvalue = dummy[botchoice].value;
+	currentvalue +=newvalue;
+	if(currentvalue>21){
+
+		document.getElementById("your-blackjack-result").innerHTML= "Busted";
+
+		}
+	else{
+	document.getElementById("your-blackjack-result").innerHTML= currentvalue;
+	document.getElementById('y_box').innerHTML=show_image;
+	}
+
+	buttonclassifier1= 200;
+}
+
+function standfunction(){
+	if(currentvalue<15){
+		alert("you Cant Stay in Less then 15 Value")
+	}
+	else{
+	let dealervalue = 0;
+
+	while(dealervalue<22){
+		if(dealervalue>14){
+			break;
+		}
+
+	botchoice = Math.random()*12;	
+	botchoice = Math.floor(botchoice);
+
+	show_image  = "<img src='blackjack_assets/images/"+dummy[botchoice].name+"'"+"style='height:78%; width:55%;padding-top:5px;'>";
+
+	let newvalue = dummy[botchoice].value;
+	dealervalue +=newvalue;
+
+	if(dealervalue>21){
+
+		document.getElementById("dealer-blackjack-result").innerHTML= "Busted";
+
+		}
+	else{
+		document.getElementById("dealer-blackjack-result").innerHTML= dealervalue;
+		document.getElementById('d_box').innerHTML=show_image;
+		}
+
+	}
+
+	if(currentvalue<22 && dealervalue<22){
+		if(currentvalue>dealervalue){
+			document.getElementById("blackjack-result").innerHTML= "You Won";
+			win+=1;
+	
+			}
+		else if(currentvalue==dealervalue){
+		
+			document.getElementById("blackjack-result").innerHTML= "Draw";
+			draw+=1;
+					}
+		else{
+			document.getElementById("blackjack-result").innerHTML= "PC Won";
+			loss+=1;
+			}
+
+		}
+	else if(currentvalue>21 && dealervalue<22){
+		document.getElementById("blackjack-result").innerHTML= "PC Won";
+		loss+=1;
+			}
+
+	else if (currentvalue<22 && dealervalue>21){
+		document.getElementById("blackjack-result").innerHTML= "You Won";
+		win+=1;
+			}
+	
+	else{
+		document.getElementById("blackjack-result").innerHTML= "Draw";
+		draw+=1;
+			}
+
+
+buttonclassifier=200;
+buttonclassifier1 = 100;
+		}
+}
+
 
 
 function blackjackgame(button_name)
 {
-let botchoice;
+
 		if(button_name.id=="hit" ){
+			
 			if(buttonclassifier==100){
-			
-		
-			botchoice = Math.random()*12;	
-			botchoice = Math.floor(botchoice);
-			show_image  = "<img src='blackjack_assets/images/"+dummy[botchoice].name+"'"+"style='height:78%; width:55%;padding-top:5px;'>";
-
-			let newvalue = dummy[botchoice].value;
-			currentvalue +=newvalue;
-			if(currentvalue>21){
-
-				document.getElementById("your-blackjack-result").innerHTML= "Busted";
-
+				hitfunction();		
 				}
-			else{
-			document.getElementById("your-blackjack-result").innerHTML= currentvalue;
-			document.getElementById('y_box').innerHTML=show_image;
 			}
-	
-		
-		buttonclassifier1= 200;
-		
-		
-			
-		
-	
-	}}
 
 		else if (button_name.id =="stand" )
 		{if( buttonclassifier1==200){
-			if(currentvalue<15){
-				alert("you Cant Stay in Less then 15 Value")
-			}
-			else{
-			let dealervalue = 0;
-	
-			while(dealervalue<22){
-				if(dealervalue>14){
-					break;
-				}
-	
-			botchoice = Math.random()*12;	
-			botchoice = Math.floor(botchoice);
+			standfunction();
 		
-			show_image  = "<img src='blackjack_assets/images/"+dummy[botchoice].name+"'"+"style='height:78%; width:55%;padding-top:5px;'>";
-
-			let newvalue = dummy[botchoice].value;
-			dealervalue +=newvalue;
-
-			if(dealervalue>21){
-
-				document.getElementById("dealer-blackjack-result").innerHTML= "Busted";
-
-				}
-			else{
-				document.getElementById("dealer-blackjack-result").innerHTML= dealervalue;
-				document.getElementById('d_box').innerHTML=show_image;
-				}
-	
 			}
-		
-			if(currentvalue<22 && dealervalue<22){
-				if(currentvalue>dealervalue){
-					document.getElementById("blackjack-result").innerHTML= "You Won";
-					win+=1;
-			
-					}
-				else if(currentvalue==dealervalue){
-				
-					document.getElementById("blackjack-result").innerHTML= "Draw";
-					draw+=1;
-							}
-				else{
-					document.getElementById("blackjack-result").innerHTML= "PC Won";
-					loss+=1;
-					}
-
-				}
-			else if(currentvalue>21 && dealervalue<22){
-				document.getElementById("blackjack-result").innerHTML= "PC Won";
-				loss+=1;
-					}
- 
-			else if (currentvalue<22 && dealervalue>21){
-				document.getElementById("blackjack-result").innerHTML= "You Won";
-				win+=1;
-					}
-			
-			else{
-				document.getElementById("blackjack-result").innerHTML= "Draw";
-				draw+=1;
-					}
-
-
-	buttonclassifier=200;
-	buttonclassifier1 = 100;
-				}
-	}}
+		}
 
 	else {
 		if(	buttonclassifier==200 && buttonclassifier1 == 100){
